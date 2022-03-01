b7148e66328f10897bc7f96801d41f96c3cfe060
codyStritz <74121962+codyStritz@users.noreply.github.com>
2022-03-01 12:23:37 -0800

    Solution

---
+++ remove-duplicates-from-sorted-array/src/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length <= 1) return nums.length;
+        int i = 0; // Slow runner
+        for (int j = 1; j < nums.length; j++) { // Fast runner
+            if (nums[j] != nums[i]) {
+                i++;    // Increment slow runner
+                nums[i] = nums[j]; // Set slow runner to value found by fast runner
+            }
+        }
+        return i + 1;
+    }
+}
